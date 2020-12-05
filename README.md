34c61f9c5ce8192d951bf892a473621a6c2b8906
Carl-Christian Sautter <mail@c-sautter.de>
2020-12-05 14:11:30 +0100

    get switch energy from fritz dect

---
+++ README.md
@@ -0,0 +1,7 @@
+# Volkszähler FritzBox API
+## Fritz!DECT 200 and 210
+### Get switch energy consumption in Wh
+````bash
+node get-switch-energy.js --ain 000012345678
+````
+AIN is the uniqe identifiere of Fritz DECT Devices.
---
+++ api/get-switch-energy.js
@@ -0,0 +1,15 @@
+require('dotenv').config({ path: '../.env' });
+var Fritz = require('fritzapi').Fritz;
+
+const optionDefinitions = [
+    { name: 'ain', type: String, multiple: false, defaultOption: true }
+]
+
+const commandLineArgs = require('command-line-args')
+const options = commandLineArgs(optionDefinitions)
+
+var f = new Fritz(process.env.FB_USER, process.env.FB_PASS, process.env.FB_URL);
+
+f.getSwitchEnergy(options['ain']).then(function(energy_wh){
+    console.log(energy_wh);
+});
