6b96abdaef365da72cb8ac312abd5462ebba0429
Carl-Christian Sautter <mail@c-sautter.de>
2021-06-30 20:44:17 +0200

    Fix - connection interrupt leads to NaN or null values

--- api/get-switch-energy.js
+++ api/get-switch-energy.js
@@ -11,9 +11,13 @@ const options = commandLineArgs(optionDefinitions)
 var f = new Fritz(process.env.FB_USER, process.env.FB_PASS, process.env.FB_URL);
 
 f.getSwitchEnergy(options['ain']).then(function(energy_wh){
-    console.log('energy ' + energy_wh);
+    if(energy_wh > 0){
+        console.log('energy ' + energy_wh);
+    }
 });
 
 f.getSwitchPower(options['ain']).then(function(power_w){
-    console.log('power ' + power_w);
+    if(power_w > 0){
+        console.log('power ' + power_w);
+    }
 });
